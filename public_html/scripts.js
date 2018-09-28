29744c81d01c9af3def13ee23f15aa842cb218f1
Edwin <erofaba@gmail.com>
2018-09-27 22:01:11 -0500

    Junte los js y la mayoría de los css

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -30,3 +30,32 @@ function editInfo(){
         c_nombre.innerHTML = cadena; //+ "<a class=\"button-edit\" id=\"edit-description-button\" onclick=\"editInfo()\">Editar</a>";
     }
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// control para los botones siguiente/anterior
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// mostrar imágenes
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    
+    if(n > slides.length) {
+        slideIndex = 1;
+    }
+    
+    if(n < 1) {
+        slideIndex = slides.length;
+    }
+    
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    
+}
