099e95b5d26728a5246ac6ecde4af8d889642dc0
Ivan Aguilar <cas12273@gmail.com>
2018-10-26 22:15:43 -0500

    Buscador agregado
    Se le agrego funcionalidad al buscador.

---
+++ public_html/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+<?php
+    if(isset($_POST['search'])) {
+        $buscar = $_POST['search'];
+        $buscar = trim($buscar);
+        $buscar = strtolower($buscar);
+        
+        header("location: searchResults.php? buscar=$buscar");
+        exit;
+    }
+?>
+<head>
+  <title>Cabecera</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Poiret+One');
+  </style>
+  <link rel="stylesheet" type="text/css" href="styles.css" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <base target="_parent">
+  <script type="text/javascript" src="scripts.js"></script>
+
+</head>
+
+<body class="header">
+  <!-- logo de la página -->
+  <div class="logo"><a href="inicio.php">W.G.T</a></div>
+
+  <!-- buscador -->
+  <form style="POSITION: absolute; margin-left: 200px;" action="" method="post" autocomplete="off" id="form-search">
+    <input id="searchBox" type="text" name="search" placeholder="Buscar en W.G.T">
+  </form>
+  <!-- botones -->
+  <nav>
+    <a href="contacto.html" title="Contactanos"><i>Contáctanos</i></a>
+    <a href="drawsView.html" title="Dibujo"><i class="fa fa-pencil"></i></a>
+    <a href="designsView.html" title="Diseño Gráfico"><i class="fa fa-desktop"></i></a>
+    <a href="3DdesignsView.html" title="Diseño 3D"><i class="fa fa-cube"></i></a>
+    <a href="login.html" title="Login"><i class="fa fa-user"></i></a>
+
+  </nav>
+
+  <div style="float: right; text-align: right"></div>
+
+</body>
+
+</html>
