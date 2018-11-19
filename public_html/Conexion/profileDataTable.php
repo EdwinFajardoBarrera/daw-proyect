6465fffced9f9f48dcb8e62d239401c3d73bfee1
Edwin <erofaba@gmail.com>
2018-11-19 11:28:15 -0600

    No funcionaron las pruebas :(

--- public_html/Conexion/profileDataTable.php
+++ public_html/Conexion/profileDataTable.php
@@ -18,15 +18,10 @@ lastname VARCHAR(50) NOT NULL,
 email VARCHAR(150) NOT NULL,
 password VARCHAR(150) NOT NULL,
 description VARCHAR(5000) NOT NULL,
--- id_profile INT(6) NOT NULL,
--- FOREIGN KEY (id_profile) REFERENCES profile(id),
+id_profile INT(6) NOT NULL,
+FOREIGN KEY (id_profile) REFERENCES profile(id),
 reg_date TIMESTAMP
-)
-
-ALTER TABLE datosPerfil
-  ADD CONSTRAINT `id_profile` 
-  FOREIGN KEY (`id_profile`) REFERENCES profile(`id`);
-";
+)";
 
 if ($conn->query($sql) === TRUE) {
     echo "Table MyGuests created successfully";
