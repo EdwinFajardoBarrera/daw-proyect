71e0185240520e51db4302f35d0a1fe0f3433686
Ivan Aguilar <cas12273@gmail.com>
2018-11-19 15:13:31 -0600

    Inicio actualizado
    Ahora las imagenes se usan desde la bdd

--- public_html/inicio.php
+++ public_html/inicio.php
@@ -6,43 +6,32 @@ and open the template in the editor.
 -->
 <html>
     <?php
-    session_start();
-    if (!isset($_SESSION['Username'])) {
-        echo "<script>
-        alert('No existe una sesión activa');
-        window.location= 'login.php'
-        </script>;";
+//    session_start();
+//    if (!isset($_SESSION['Username'])) {
+//        echo "<script>
+//        alert('No existe una sesión activa');
+//        window.location= 'login.php'
+//        </script>;";
+//    }
+    
+    require './Conexion/Conexion.php';
+    
+    $ctrlConexion = new Conexion();
+    $conexion = $ctrlConexion->startConexion();
+    $consulta = "SELECT * FROM images";
+    $resultado = $conexion->query($consulta);  
+    
+    $numImagen = 0;
+    while($columna = $resultado->fetch_assoc()) {  
+        $todasLasImagenes[$numImagen] = '<div class="mySlides fade">
+                                            <img src="DB/'. $columna["imageType"] . '/' . $columna["imageName"] . '' . $columna["imageExtension"] .'" width="100%" height="500">
+                                           <iframe class="area-comentarios" src="comentarios.php" frameborder="0" width="100%" height="90"></iframe>
+                                         </div>';
+        $numImagen++;
     }
     
-    $RUTA_ARCHIVO = '../all_images.txt';
-    $fpDatos = fopen($RUTA_ARCHIVO, 'r');
-    if (!$fpDatos) {
-        echo 'ERROR: No ha sido posble encontrar el archivo.';
-        exit;
-    }
-
-    $contador = 0;
-    $numImagen = 0;
-    while (!feof($fpDatos)) {
-        $linea = fgets($fpDatos);
-        $field[$contador] = explode('|', $linea);
-
-        foreach ($field[$contador] as $imagen) {
-
-            $imagen = trim($imagen);
-
-            if ($imagen != "") {
-                $todasLasImagenes[$numImagen] = '<div class="mySlides fade">
-                                            <img src="DB/all_images/' . $imagen . '.jpg" width="100%" height="500">
-                                            <iframe class="area-comentarios" src="comentarios.html" frameborder="0" width="100%" height="90"></iframe>
-                                          </div>';
-                $numImagen++;
-            }
-        }
-
-        $fpDatos++;
-    }
     ?>
+    
     <head>
         <title>Inicio</title>
         <meta charset="UTF-8">
